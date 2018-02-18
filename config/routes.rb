c3c483eb2e4db4a8f78a2d970e043fa41ddb308f
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2018-02-18 14:44:21 -0600

    add navigation for Users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
 
   devise_for :users, controllers: { registrations: 'registrations'}
-  get 'users/:id', to: "users#show"
+  get 'users/:id', to: "users#show", as: :view_profile
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
