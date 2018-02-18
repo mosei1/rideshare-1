4ef80a5c7dc9352af8d79e9064d11408a111d20f
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2018-02-17 20:28:08 -0600

    create join between Users and Rides

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,4 @@
 class Ride < ApplicationRecord
   belongs_to :driver, class_name: "User"
+  has_and_belongs_to_many :riders, class_name: "User"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180218022450) do
+ActiveRecord::Schema.define(version: 20180218022640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20180218022450) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_rides_on_user_id"
+  end
+
+  create_table "rides_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "ride_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
