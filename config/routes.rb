e06461b6bb6cb8be7071e48838e49af7bb3cf60f
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2018-02-17 20:45:23 -0600

    persist User info with devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :rides
 
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations'}
   get 'users/:id', to: "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
